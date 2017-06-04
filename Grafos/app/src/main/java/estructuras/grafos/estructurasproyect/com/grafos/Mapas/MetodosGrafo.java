032dd34396852da6dfe3fc4e8f3ac6ee1a8437bc
Angelo Guerrero Medina <angelomedinabarraza@gmail.com>
2017-06-04 10:54:13 -0600

    Cambios

---
+++ Grafos/app/src/main/java/estructuras/grafos/estructurasproyect/com/grafos/Mapas/MetodosGrafo.java
@@ -0,0 +1,36 @@
+package estructuras.grafos.estructurasproyect.com.grafos.Mapas;
+
+/**
+ * Created by Usuario on 4/6/2017.
+ */
+
+public class MetodosGrafo {
+
+
+    //insertar vertce
+
+    // modificar
+
+     //eliminar
+
+    //insertar arco
+
+    // tipo de grafo
+
+    // camino ma corto
+
+    // conexo
+
+    // ciclico
+
+    // llegar de A a B
+
+    // vertices sumideros
+
+    // vertices fuentes
+
+    // grado externo
+
+    // grado interno
+
+}
---
+++ Grafos/app/src/main/java/estructuras/grafos/estructurasproyect/com/grafos/Singleton/SingletonGrafo.java
@@ -0,0 +1,23 @@
+package estructuras.grafos.estructurasproyect.com.grafos.Singleton;
+
+import estructuras.grafos.estructurasproyect.com.grafos.Mapas.MetodosGrafo;
+
+/**
+ * Created by Usuario on 4/6/2017.
+ */
+
+public class SingletonGrafo {
+
+    public MetodosGrafo metGrafo=new MetodosGrafo();//estoy instanciando la los metodos de grafo
+
+    private static SingletonGrafo _instance;
+
+    public static SingletonGrafo getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SingletonGrafo();
+        }
+        return _instance;
+    }
+}
