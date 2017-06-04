7d030b4aa72be42462aa04f275e415df5b218a3a
Angelo Guerrero Medina <angelomedinabarraza@gmail.com>
2017-06-04 11:53:11 -0600

    Metodos Grafos

--- Grafos/app/src/main/java/estructuras/grafos/estructurasproyect/com/grafos/Mapas/MetodosGrafo.java
+++ Grafos/app/src/main/java/estructuras/grafos/estructurasproyect/com/grafos/Mapas/MetodosGrafo.java
@@ -1,10 +1,82 @@
 package estructuras.grafos.estructurasproyect.com.grafos.Mapas;
 
+import java.util.ArrayList;
+
 /**
  * Created by Usuario on 4/6/2017.
  */
 
 public class MetodosGrafo {
+
+
+
+    //Grafo
+    public Vertice grafoCreado;
+    public ArrayList<String> lista_vertices_agregados = new ArrayList<>();
+    public boolean vertice_existente;
+
+
+    public Vertice buscarVertice(String nombre){
+        if(grafoCreado == null){
+            return null;
+        }
+        Vertice aux = grafoCreado;
+
+        while(aux != null){
+            if(aux.nombre.equals(nombre)){
+                return aux;
+            }
+            aux = aux.sigVertice;
+        }
+        return null;
+    }
+
+    public void InsertarVertice(Vertice nuevoV){
+        if (grafoCreado == null){
+            grafoCreado = nuevoV;
+            //Toast.makeText(MapsActivityCreaGrafo.this, "Agregado (Raiz)", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Vertice aux = grafoCreado;
+        while (aux != null){
+            if (aux.sigVertice == null){
+                aux.sigVertice = nuevoV;
+                //.makeText(MapsActivityCreaGrafo.this, "Agregado!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            aux = aux.sigVertice;
+        }
+    }
+
+    public void InsertarArco(String nombre, Arco nuevoA){
+        if (grafoCreado == null){
+            return;
+        }
+        Vertice auxV = grafoCreado;
+
+        while(auxV != null){
+            if(auxV.nombre.equals(nombre)){
+                if(auxV.sigA == null){
+                    auxV.sigA = nuevoA;
+                    //Toast.makeText(MapsActivityCreaGrafo.this, "Arco primero", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Arco auxA = auxV.sigA;
+
+                while (auxA != null){
+                    if(auxA.sigArco == null){
+                        auxA.sigArco = nuevoA;
+                        //Toast.makeText(MapsActivityCreaGrafo.this, "Agregado!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    auxA = auxA.sigArco;
+                }
+            }
+            auxV = auxV.sigVertice;
+        }
+    }
+
 
 
     //insertar vertce
